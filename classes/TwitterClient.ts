7e265608d710e326394cdb883b734b0c0488bfae
Ori Granot <oriorigranot@gmail.com>
2021-05-25 18:01:13 +0300

    Changed the twitter client

--- classes/TwitterClient.ts
+++ classes/TwitterClient.ts
@@ -9,13 +9,13 @@ export class TwitterClient {
         this.client = new Twitter({ consumer_key, consumer_secret, access_token_key, access_token_secret });
     }
 
-    newGemAlert = async (coin: Coin, link: string) => {
+    newGemAlert = (coin: Coin, link: string) => {
         let tweetString: String = `🚨 New ${coin.site} Listing 🚨\n` +
             `${coin.name} / $${coin.symbol}\n\n` +
             `#Defi #BSC #${coin.site}\n` +
             `${link}`;
 
-        await this.client.post("statuses/update", {
+        this.client.post("statuses/update", {
             status: tweetString
         }).catch((err: any) => {
             console.error(err);
